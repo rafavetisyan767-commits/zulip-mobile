0481d27a555769db59ac278c84186b4b7fe3c43a
rafavetisyan767-commits <rafavetisyan767@gmail.com>
2026-05-04 16:01:27 +0400

    Remove Home and Subscriptions screens from tabs

--- src/main/MainTabsScreen.js
+++ src/main/MainTabsScreen.js
@@ -11,17 +11,13 @@ import { getUnreadHuddlesTotal, getUnreadPmsTotal } from '../selectors';
 import { useSelector } from '../react-redux';
 import type { AppNavigationMethods, AppNavigationProp } from '../nav/AppNavigator';
 import { bottomTabNavigatorConfig } from '../styles/tabs';
-import HomeScreen from './HomeScreen';
 import PmConversationsScreen from '../pm-conversations/PmConversationsScreen';
-import { IconInbox, IconStream, IconPeople } from '../common/Icons';
+import { IconPeople } from '../common/Icons';
 import OwnAvatar from '../common/OwnAvatar';
 import ProfileScreen from '../account-info/ProfileScreen';
 import styles, { BRAND_COLOR, ThemeContext } from '../styles';
-import SubscriptionsScreen from '../streams/SubscriptionsScreen';
 
 export type MainTabsNavigatorParamList = {|
-  +home: RouteParamsOf<typeof HomeScreen>,
-  +subscribed: RouteParamsOf<typeof SubscriptionsScreen>,
   +'pm-conversations': RouteParamsOf<typeof PmConversationsScreen>,
   +profile: RouteParamsOf<typeof ProfileScreen>,
 |};
@@ -29,10 +25,7 @@ export type MainTabsNavigatorParamList = {|
 export type MainTabsNavigationProp<
   +RouteName: $Keys<MainTabsNavigatorParamList> = $Keys<MainTabsNavigatorParamList>,
 > =
-  // Screens on this navigator will get a `navigation` prop that reflects
-  // this navigator itself…
   BottomTabNavigationProp<MainTabsNavigatorParamList, RouteName> &
-    // … plus the methods it gets from its parent navigator.
     AppNavigationMethods;
 
 const Tab = createBottomTabNavigator<
@@ -48,33 +41,16 @@ type Props = $ReadOnly<{|
 
 export default function MainTabsScreen(props: Props): Node {
   const { backgroundColor } = useContext(ThemeContext);
-
   const unreadPmsCount = useSelector(getUnreadHuddlesTotal) + useSelector(getUnreadPmsTotal);
 
   return (
     <View style={[styles.flexed, { backgroundColor }]}>
       <Tab.Navigator {...bottomTabNavigatorConfig()} lazy={false} backBehavior="none">
         <Tab.Screen
-          name="home"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color }) => <IconInbox size={24} color={color} />,
-          }}
-        />
-        <Tab.Screen
-          name="subscribed"
-          component={SubscriptionsScreen}
-          options={{
-            tabBarLabel: 'Streams',
-            tabBarIcon: ({ color }) => <IconStream size={24} color={color} />,
-          }}
-        />
-        <Tab.Screen
           name="pm-conversations"
           component={PmConversationsScreen}
           options={{
-            tabBarLabel: 'Direct messages',
+            tabBarLabel: 'Messages',
             tabBarIcon: ({ color }) => <IconPeople size={24} color={color} />,
             tabBarBadge: unreadPmsCount > 0 ? unreadPmsCount : undefined,
             tabBarBadgeStyle: {
